c92257e033a40951c8cc2addddfde525157450ec
Ben Hsieh <bqh5026@gmail.com>
2020-08-26 13:32:58 -0400

    Convert line chart to bar chart

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Line } from 'react-chartjs-2'; 
+import { Bar } from 'react-chartjs-2'; 
 
 function LineChart({ data, labels }) {
   const initData = {
@@ -7,7 +7,7 @@ function LineChart({ data, labels }) {
     datasets: data,
   };
 
-  return <Line data={initData} />;
+  return <Bar data={initData} />;
 }
 
 export default LineChart; 
